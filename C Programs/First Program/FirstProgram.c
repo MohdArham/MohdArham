5a1b92294d73e8cb963f8eed9b0a98652df7febd
MohdArham <mohdarham94@gmail.com>
2022-09-17 15:25:35 +0530

    add

---
+++ C Programs/First Program/FirstProgram.c
@@ -0,0 +1,7 @@
+# include<stdio.h>
+//This is my first c program
+int main()
+  {
+    printf("Hello ! My name is Mohd Arham.");
+    return 0;
+  }
---
+++ C Programs/Sum of 2 numbers/Sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>    
+int main()
+{
+
+int x=0,y=0,result=0;  
+  
+printf("Enter the first number:");  
+scanf("%d",&x);  
+
+printf("Enter the second number:");  
+scanf("%d",&y);  
+  
+result=x+y;  
+printf("The sum of 2 numbers is:%d ",result);  
+  
+return 0;  
+}    
